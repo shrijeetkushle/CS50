f9c18386f23069e151e084c73080163d553cbeb1
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-04-11 22:28:28 +0530

    Create mario.c

---
+++ Week 1/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void){
+int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
